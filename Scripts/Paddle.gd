baffe640f4166069160be3faec4a626891e1b161
Kenee Patel <patelkenee2804@gmail.com>
2020-12-28 20:51:41 +0530

    added left right mouse click inputs

--- Scripts/Paddle.gd
+++ Scripts/Paddle.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-var vel : int = 1
-const max_displacement : int = 190
+var vel : float = 1
+const max_displacement : int = 192
 
 func _process(delta):
 	position.x += vel * delta * 60
@@ -11,9 +11,23 @@ func _process(delta):
 			vel = -5
 		elif Input.is_action_pressed("ui_right"):
 			vel = 5
+		else:
+			if vel > 0: vel -= 0.25
+			elif vel < 0: vel += 0.25
 	else:
 		vel = 0
 		if position.x <= max_displacement:
 			position.x += 1
 		else:
 			position.x -= 1
+			
+	if InputEventScreenTouch:
+		print()
+
+func _input(event):
+	if event is InputEventMouseButton and event.position.x < 240:
+		vel = -5
+		print(event.position)
+	if event is InputEventMouseButton and event.position.x > 240:
+		vel = +5
+		print(event.position)
