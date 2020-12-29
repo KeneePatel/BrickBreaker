23ac5b6a45fb942db89007ec7f8e84582ed894e2
Kenee Patel <patelkenee2804@gmail.com>
2020-12-29 16:21:24 +0530

    Dynamicity and better ball direction and speed updates

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -1,7 +1,22 @@
 extends RigidBody2D
 
-func _process(_delta):
+const speed_up : int = 5
+const max_speed : int = 400
+
+func _ready():
+	set_physics_process(true)
+
+func _physics_process(delta):
 	var body = get_colliding_bodies()
 	if !body.empty():
 		if body[0].is_in_group("Bricks"):
 			body[0].queue_free()
+		if body[0].get_name() == "Paddle":
+			var speed = get_linear_velocity().length()
+			#print("ball position: ", get_position())
+			#print("paddle position: ", body[0].get_node("Anchor").get_global_position())
+			var direction = get_position() - body[0].get_node("Anchor").get_global_position()
+			#print("Direction: ", direction)
+			#print("unit Direction: ", direction.normalized()) # unit vector for direction
+			var velocity = direction.normalized()*min(speed + speed_up, max_speed*delta)*60
+			set_linear_velocity(velocity)
--- Scripts/Paddle.gd
+++ Scripts/Paddle.gd
@@ -1,12 +1,15 @@
 extends KinematicBody2D
 
 var vel : float = 1
-const max_displacement : int = 192
+const max_displacement : int = 200
 
-func _process(delta):
+func _ready():
+	set_physics_process(true)
+
+func _physics_process(delta):
 	position.x += vel * delta * 60
 	
-	if position.x <= max_displacement and position.x >= -max_displacement:
+	if position.x <= 240+max_displacement and position.x >= 240-max_displacement:
 		if Input.is_action_pressed("ui_left"):
 			vel = -5
 		elif Input.is_action_pressed("ui_right"):
@@ -16,13 +19,13 @@ func _process(delta):
 			elif vel < 0: vel += 0.25
 	else:
 		vel = 0
-		if position.x <= max_displacement:
+		if position.x <= 240+max_displacement:
 			position.x += 1
 		else:
 			position.x -= 1
-			
-	if InputEventScreenTouch:
-		print()
+
+#	if InputEventScreenTouch:
+#		print()
 
 #func _input(event):
 #	if event is InputEventMouseButton and event.position.x < 240:
